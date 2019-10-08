dc13616c812bf1703f1ef57b9a8aba324fd278b9
Brendan Burns <bburns@microsoft.com>
2019-10-08 12:37:55 -0700

    Initial add.

---
+++ examples/simple/app.js
@@ -0,0 +1,102 @@
+var dapr = require('dapr-client');
+var messages = dapr.dapr_pb; 
+var services = dapr.dapr_grpc;
+var grpc = require('grpc');
+
+// TODO: Get port from the environment.
+var client = new services.DaprClient(
+    `localhost:50001`, grpc.credentials.createInsecure());
+
+var event = new messages.PublishEventEnvelope();
+event.setTopic('sith');
+
+var data = new proto.google.protobuf.Any();
+data.setValue(Buffer.from('lala'));
+event.setData(data);
+
+client.publishEvent(event, (err, response) => {
+    if (err) {
+        console.log(`Error publishing! ${err}`);
+    } else {
+        console.log('Published!');
+    }
+});
+
+// Invoke output binding named 'storage'
+var binding = new messages.InvokeBindingEnvelope();
+binding.setName('storage');
+binding.setData(data);
+
+client.invokeBinding(binding, (err, response) => {
+    if (err) {
+        console.log(`Error binding: ${err}`);
+    } else {
+        console.log('Bound!');
+    }
+});
+
+
+var invoke = new messages.InvokeServiceEnvelope();
+invoke.setId('grpcapp');
+invoke.setMethod('sith');
+var serialized = new proto.google.protobuf.Any();
+serialized.setValue(Buffer.from(JSON.stringify({
+    name: 'test',
+    message: {
+        counter: 1
+    }
+})));
+invoke.setData(serialized);
+
+client.invokeService(invoke, (err, response) => {
+    if (err) {
+        console.log(`Error invoking service: ${err}`);
+    } else {
+        console.log('Invoked!');
+    }
+});
+
+var key = 'mykey';
+
+var state = new messages.SaveStateEnvelope();
+var req = new messages.StateRequest();
+req.setKey(key);
+
+var value = new proto.google.protobuf.Any();
+value.setValue(Buffer.from('My State'));
+req.setValue(value);
+
+state.addRequests(req);
+
+client.saveState(state, (err, res) => {
+    if (err) {
+        console.log(`Error saving state: ${err}`);
+    } else {
+        console.log('Saved!');
+
+        // saved, now do a get, promises would clean this up...
+        var get = new messages.GetStateEnvelope();
+        get.setKey(key);
+        client.getState(get, (err, response) => {
+            if (err) {
+                console.log(`Error getting state: ${err}`);
+            } else {
+                console.log('Got!');
+                console.log(response.getData().getValue());
+
+                // get done, now delete, again promises would be nice...
+                var del = new messages.DeleteStateEnvelope();
+                del.setKey(key);
+                client.deleteState(del, (err, response) => {
+                    if (err) {
+                        console.log(`Error deleting state: ${err}`);
+                    } else {
+                        console.log('Deleted!');
+                    }
+                });
+            }
+        });
+    }
+});
+
+
