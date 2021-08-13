d09e79b6b1fdb76e1496af8c8812cef67d925da4
xavier geerinck <xavier.geerinck@gmail.com>
2021-08-13 07:06:50 +0000

    Cleaning up tests and dependencies

--- test/unit/actor/actor.test.ts
+++ test/unit/actor/actor.test.ts
@@ -1,25 +1,26 @@
-import { v4 as uuidv4 } from "uuid";
-import { DaprClient } from '../../../src';
+import { DaprClient, Temporal } from '../../../src';
 import ActorRuntime from '../../../src/actors/runtime/ActorRuntime';
 import ActorRuntimeConfig from '../../../src/actors/runtime/ActorRuntimeConfig';
-import ActorId from "../../../src/actors/ActorId";
-import FakeSimpleActorImpl from '../../actor/FakeSimpleActorImpl';
-import FakeSimpleTimerActorImpl from '../../actor/FakeSimpleTimerActorImpl';
 
 describe('Actor', () => {
     let client: DaprClient;
 
     beforeAll(async () => {
-        jest.mock('../../src/DaprClient'); // converts DaprClient into a mock constructor
-        client = new DaprClient("", "");
+        jest.mock('../../../src/implementation/Client/DaprClient'); // converts DaprClient into a mock constructor
+        client = new DaprClient("127.0.0.1", "1000");
     });
 
     beforeEach(async () => {
-        const runtime = ActorRuntime.getInstance(client);
+        const runtime = ActorRuntime.getInstanceByDaprClient(client);
 
         // Reset the runtime config
         const config = runtime.getActorRuntimeConfig();
-        const newConfig = new ActorRuntimeConfig(1 * 60 * 60, 30, 1 * 60, true);
+        const newConfig = new ActorRuntimeConfig(
+          Temporal.Duration.from({ hours: 1 })
+          , Temporal.Duration.from({ seconds: 30 })
+          , Temporal.Duration.from({ minutes: 1 })
+          , true
+        );
         runtime.setActorRuntimeConfig(newConfig);
 
         // Clear the Actor Managers
--- test/unit/actor/actorRuntime.test.ts
+++ test/unit/actor/actorRuntime.test.ts
@@ -1,98 +1,104 @@
 import { v4 as uuidv4 } from "uuid";
-import { DaprClient } from '../../src';
-import ActorRuntime from '../../src/actors/runtime/ActorRuntime';
-import ActorRuntimeConfig from '../../src/actors/runtime/ActorRuntimeConfig';
-import ActorId from "../../src/actors/ActorId";
-import FakeSimpleActorImpl from '../actor/FakeSimpleActorImpl';
-import FakeSimpleTimerActorImpl from '../actor/FakeSimpleTimerActorImpl';
+import { DaprClient, Temporal } from '../../../src';
+import ActorRuntime from '../../../src/actors/runtime/ActorRuntime';
+import ActorRuntimeConfig from '../../../src/actors/runtime/ActorRuntimeConfig';
+import ActorId from "../../../src/actors/ActorId";
+import DemoActorCounterImpl from '../../actor/DemoActorCounterImpl';
+import DemoActorSayImpl from '../../actor/DemoActorSayImpl';
 
 describe('ActorRuntime', () => {
-    let client: DaprClient;
+  let client: DaprClient;
+  let runtime: ActorRuntime;
 
-    beforeAll(async () => {
-        jest.mock('../../src/DaprClient'); // converts DaprClient into a mock constructor
-        client = new DaprClient("", "");
-    });
+  beforeAll(async () => {
+    jest.mock('../../../src/implementation/Client/DaprClient'); // converts DaprClient into a mock constructor
+    client = new DaprClient("127.0.0.1", "1000");
+  });
 
-    beforeEach(async () => {
-        const runtime = ActorRuntime.getInstance(client);
+  beforeEach(async () => {
+    runtime = ActorRuntime.getInstanceByDaprClient(client);
 
-        // Reset the runtime config
-        const config = runtime.getActorRuntimeConfig();
-        const newConfig = new ActorRuntimeConfig(1 * 60 * 60, 30, 1 * 60, true);
-        runtime.setActorRuntimeConfig(newConfig);
+    // Reset the runtime config
+    const config = runtime.getActorRuntimeConfig();
+    const newConfig = new ActorRuntimeConfig(
+      Temporal.Duration.from({ hours: 1 })
+      , Temporal.Duration.from({ seconds: 30 })
+      , Temporal.Duration.from({ minutes: 1 })
+      , true
+    );
+    runtime.setActorRuntimeConfig(newConfig);
 
-        // Clear the Actor Managers
-        runtime.clearActorManagers();
-    });
+    // Clear the Actor Managers
+    runtime.clearActorManagers();
+  });
 
-    it('should have an actor config that matches the default values', async () => {
-        const runtime = ActorRuntime.getInstance(client);
-        const config = runtime.getActorRuntimeConfig();
-        expect(config.getActorIdleTimeout()).toEqual(1 * 60 * 60);
-        expect(config.getActorScanInterval()).toEqual(30);
-        expect(config.getDrainOngoingCallTimeout()).toEqual(1 * 60);
-        expect(config.getDrainRebalancedActors()).toEqual(true);
-    });
+  it('should have an actor config that matches the default values', async () => {
+    const config = runtime.getActorRuntimeConfig();
+    expect(config.getActorIdleTimeout()).toEqual(1 * 60 * 60);
+    expect(config.getActorScanInterval()).toEqual(30);
+    expect(config.getDrainOngoingCallTimeout()).toEqual(1 * 60);
+    expect(config.getDrainRebalancedActors()).toEqual(true);
+  });
 
-    it('should allow us to change the actor runtime config values', async () => {
-        const runtime = ActorRuntime.getInstance(client);
-        const newConfig = new ActorRuntimeConfig(3 * 60 * 60, 10, 2 * 60, false);
-        runtime.setActorRuntimeConfig(newConfig);
+  it('should allow us to change the actor runtime config values', async () => {
+    const newConfig = new ActorRuntimeConfig(
+      Temporal.Duration.from({ hours: 3 })
+      , Temporal.Duration.from({ seconds: 10 })
+      , Temporal.Duration.from({ minutes: 2 })
+      , false
+    );
 
-        const config = runtime.getActorRuntimeConfig();
-        expect(config.getActorIdleTimeout()).toEqual(3 * 60 * 60);
-        expect(config.getActorScanInterval()).toEqual(10);
-        expect(config.getDrainOngoingCallTimeout()).toEqual(2 * 60);
-        expect(config.getDrainRebalancedActors()).toEqual(false);
-    });
+    runtime.setActorRuntimeConfig(newConfig);
 
-    it('should be able to register an actor', async () => {
-        const runtime = ActorRuntime.getInstance(client);
-        await runtime.registerActor(FakeSimpleActorImpl);
-        await runtime.registerActor(FakeSimpleTimerActorImpl);
+    const config = runtime.getActorRuntimeConfig();
+    expect(config.getActorIdleTimeout()).toEqual(3 * 60 * 60);
+    expect(config.getActorScanInterval()).toEqual(10);
+    expect(config.getDrainOngoingCallTimeout()).toEqual(2 * 60);
+    expect(config.getDrainRebalancedActors()).toEqual(false);
+  });
 
-        expect(runtime.getRegisteredActorTypes().indexOf(FakeSimpleActorImpl.name)).toBeGreaterThan(-1);
-        expect(runtime.getRegisteredActorTypes().indexOf(FakeSimpleTimerActorImpl.name)).toBeGreaterThan(-1);
-    });
+  it('should be able to register an actor', async () => {
+    await runtime.registerActor(DemoActorCounterImpl);
+    await runtime.registerActor(DemoActorSayImpl);
 
-    it('should only register an actor once', async () => {
-        const runtime = ActorRuntime.getInstance(client);
-        await runtime.registerActor(FakeSimpleActorImpl);
-        await runtime.registerActor(FakeSimpleActorImpl);
+    expect(runtime.getRegisteredActorTypes().indexOf(DemoActorCounterImpl.name)).toBeGreaterThan(-1);
+    expect(runtime.getRegisteredActorTypes().indexOf(DemoActorSayImpl.name)).toBeGreaterThan(-1);
+  });
 
-        expect(runtime.getRegisteredActorTypes().indexOf(FakeSimpleActorImpl.name)).toBeGreaterThan(-1);
-        expect(runtime.getRegisteredActorTypes().length).toEqual(1);
-    });
+  it('should only register an actor once', async () => {
+    await runtime.registerActor(DemoActorCounterImpl);
+    await runtime.registerActor(DemoActorCounterImpl);
 
-    it('should be able to invoke an actor', async () => {
-        const actorId = uuidv4();
+    expect(runtime.getRegisteredActorTypes().indexOf(DemoActorCounterImpl.name)).toBeGreaterThan(-1);
+    expect(runtime.getRegisteredActorTypes().length).toEqual(1);
+  });
 
-        const runtime = ActorRuntime.getInstance(client);
-        await runtime.registerActor(FakeSimpleActorImpl);
+  it('should be able to invoke an actor', async () => {
+    const actorId = uuidv4();
 
-        const res = await runtime.invoke(FakeSimpleActorImpl.name, actorId, "sayMessage", Buffer.from("Hello World"));
-        expect(res[0].toString()).toEqual("Hello World");
-    });
+    await runtime.registerActor(DemoActorCounterImpl);
 
-    it('should receive an error if the actor method does not exist', async () => {
-        const actorId = uuidv4();
+    const res = await runtime.invoke(DemoActorCounterImpl.name, actorId, "sayMessage", Buffer.from("Hello World"));
+    expect(res[0].toString()).toEqual("Hello World");
+  });
 
-        const runtime = ActorRuntime.getInstance(client);
-        await runtime.registerActor(FakeSimpleActorImpl);
+  it('should receive an error if the actor method does not exist', async () => {
+    const actorId = uuidv4();
 
-        try {
-            await runtime.invoke(FakeSimpleActorImpl.name, actorId, "someRandomMethod");
-        } catch (e) {
-            expect(e.message).toEqual(`{"error":"ACTOR_METHOD_DOES_NOT_EXIST","errorMsg":"The actor method 'someRandomMethod' does not exist on FakeSimpleActorImpl"}`);
-        }
-    });
+    await runtime.registerActor(DemoActorCounterImpl);
 
-    it('should be able to fire a reminder', async () => {
-        const actorId = new ActorId(uuidv4());
-    });
+    try {
+      await runtime.invoke(DemoActorCounterImpl.name, actorId, "someRandomMethod");
+    } catch (e) {
+      expect(e.message).toEqual(`{"error":"ACTOR_METHOD_DOES_NOT_EXIST","errorMsg":"The actor method 'someRandomMethod' does not exist on ${DemoActorCounterImpl.name}"}`);
+    }
+  });
 
-    it('should be able to fire a timer', async () => {
-        const actorId = new ActorId(uuidv4());
-    });
+  it('should be able to fire a reminder', async () => {
+    const actorId = new ActorId(uuidv4());
+  });
+
+  it('should be able to fire a timer', async () => {
+    const actorId = new ActorId(uuidv4());
+  });
 })
