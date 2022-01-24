b2d6787305c698c82f7f711eb2498dc6c3ddc643
Hannah Hunter <hannahhunter@microsoft.com>
2022-01-24 17:42:06 -0600

    update with js sdk 'get config keys'
    Signed-off-by: Hannah Hunter <hannahhunter@microsoft.com>

---
+++ examples/configuration/src/index.ts
@@ -0,0 +1,24 @@
+import { DaprClient, CommunicationProtocolEnum } from "dapr-client";
+
+// Common settings
+const daprHost = "127.0.0.1";
+
+async function start() {
+  // Note that the DAPR_HTTP_PORT and DAPR_GRPC_PORT environment variables are set by DAPR itself. https://docs.dapr.io/reference/environment/
+
+  const client = new DaprClient(
+    daprHost,
+    process.env.DAPR_HTTP_PORT as string,
+    CommunicationProtocolEnum.HTTP
+  );
+
+  const config = await client.configuration.get('config-store', ['key1', 'key2']);
+  console.log(config); 
+
+  console.log(JSON.stringify(config));
+}
+
+start().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
