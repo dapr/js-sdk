3728bccd7976691cd4d9e4cd473fcfc9cc5baad5
Shubham Sharma <shubhash@microsoft.com>
2022-02-16 19:21:37 +0530

    Add tests
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

---
+++ test/unit/utils/Settings.util.test.ts
@@ -0,0 +1,25 @@
+import { CommunicationProtocolEnum } from "../../../src";
+import { Settings } from "../../../src/utils/Settings.util";
+
+describe('Settings', () => {
+    describe('getDefaultPort returns the correct default when', () => {
+        it('communication protocol is GRPC', () => {
+            expect(Settings.getDefaultPort(CommunicationProtocolEnum.GRPC))
+                .toEqual(Settings.getDefaultGrpcPort())
+        });
+        it('communication protocol is HTTP', () => {
+            expect(Settings.getDefaultPort(CommunicationProtocolEnum.HTTP))
+                .toEqual(Settings.getDefaultHttpPort())
+        });
+    });
+    describe('getDefaultAppPort returns the correct default when', () => {
+        it('communication protocol is GRPC', () => {
+            expect(Settings.getDefaultAppPort(CommunicationProtocolEnum.GRPC))
+                .toEqual(Settings.getDefaultGrpcAppPort())
+        });
+        it('communication protocol is HTTP', () => {
+            expect(Settings.getDefaultAppPort(CommunicationProtocolEnum.HTTP))
+                .toEqual(Settings.getDefaultHttpAppPort())
+        });
+    });
+})
