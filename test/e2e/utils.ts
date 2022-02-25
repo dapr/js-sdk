f8a8baa52da222c70de67e146fba8a82055afb68
Shubham Sharma <shubhash@microsoft.com>
2022-02-25 17:47:37 +0530

    Use file instead of env var
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- test/e2e/utils.ts
+++ test/e2e/utils.ts
@@ -1,12 +1,21 @@
+var fs = require("fs");
+
+const errorFilePath = "E2E_TEST_ERROR";
+
 export async function testIt(name: string, fn?: any, timeout?: number): Promise<void> {
     test(name, async () => {
         try {
             await fn()
         }
         catch (error) {
-            // set failure environment variable
-            process.env.E2E_TEST_FAILURE = "true";
+            // Create a new file to denote error.
+            touchFile(errorFilePath);
             throw error
         }
     }, timeout)
+}
+
+// Creates an empty file at filepath.
+function touchFile(filepath: string) {
+    fs.write(fs.openSync(filepath, 'w'));
 }
