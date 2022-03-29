7541ca2837d11d4be5d327330c4aaaa4f37a23b2
Shubham Sharma <shubhash@microsoft.com>
2022-03-29 09:02:23 +0530

    Refactor based on reviews
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- .github/scripts/dapr_bot.js
+++ .github/scripts/dapr_bot.js
@@ -1,71 +1,185 @@
+// list of owner who can control dapr-bot workflow.
+// TODO: Read owners from OWNERS file.
+const owners = [
+    "yaron2",
+    "youngbupark",
+    "Haishi2016",
+    "lukekim",
+    "amanbha",
+    "msfussell",
+    "shalabhms",
+    "LMWF",
+    "artursouza",
+    "vinayada1",
+    "mukundansundar",
+    "wcs1only",
+    "orizohar",
+    "pruthvidhodda",
+    "mchmarny",
+    "tcnghia",
+    "berndverst",
+    "halspang",
+    "tanvigour",
+    "dmitsh",
+    "pkedy",
+    "CodeMonkeyLeet",
+    "XavierGeerinck",
+    "amulyavarote",
+    "shubham1172"
+];
+
 /**
  * Execute fn if label exists on an issue.
  * @param {*} github GitHub object reference
- * @param {*} label label name
  * @param {*} issue GitHub issue reference
+ * @param {*} label label name
  * @param {*} fn async function
  */
  async function executeIfIssueHasLabel(github, issue, label, fn) {
-    var response = await github.issues.listLabelsOnIssue({
+    const response = await github.issues.listLabelsOnIssue({
         issue_number: issue.number,
         owner: issue.owner,
         repo: issue.repo,
     });
 
-    var labelNames = response.data.map((i) => i.name)
-    for (const labelName of labelNames) {
-        if (labelName == label) {
-            await fn()
-        }
+    const labelNames = response.data.map((i) => i.name);
+    if (labelNames.indexOf(label) > -1) {
+        await fn();
+    }
+}
+
+/**
+ * Assign an issue to assignee.
+ * @param {*} github GitHub object reference
+ * @param {*} context GitHub action context
+ * @param {boolean} isPullRequest is the workflow triggered by a pull request?
+ */
+async function executeAssign(github, context, isPullRequest) {
+    const issue = context.issue;
+
+    if (isPullRequest) {
+        console.log("[executeAssign] pull requests unsupported, skipping command execution.");
+        return;
+    } else if (issue.assignees && issue.assignees.length !== 0) {
+        console.log("[executeAssign] issue already has assignees, skipping command execution.");
+        return;
+    }
+
+    await github.issues.addAssignees({
+        owner: issue.owner,
+        repo: issue.repo,
+        issue_number: issue.number,
+        assignees: [context.actor],
+    });
+}
+
+/**
+ * Add a label to the issue indicating that it needs the author's feedback.
+ * @param {*} github GitHub object reference
+ * @param {*} context GitHub action context
+ * @param {boolean} isPullRequest is the workflow triggered by a pull request?
+ * @returns 
+ */
+async function executePingAuthor(github, context, isPullRequest) {
+    const issue = context.issue;
+
+    if (isPullRequest) {
+        console.log("[executePingAuthor] pull requests unsupported, skipping command execution.");
+        return;
+    } else if (owners.indexOf(context.actor) < 0) {
+        console.log("[executePingAuthor] user does not have privilege, skipping command execution.");
+        return;
+    }
+
+    // if there is a 'needs-team-attention' label, remove it.
+    await executeIfIssueHasLabel(github, issue, 'needs-team-attention', async () => {
+        await github.issues.removeLabel({
+            issue_number: issue.number,
+            owner: issue.owner,
+            repo: issue.repo,
+            name: 'needs-team-attention'
+        });
+    });
+
+    // Add new label
+    await github.issues.addLabels({
+        issue_number: issue.number,
+        owner: issue.owner,
+        repo: issue.repo,
+        labels: ['needs-author-feedback']
+    });
+}
+
+/**
+ * Trigger e2e tests for pull request
+ * @param {*} github GitHub object reference
+ * @param {*} context GitHub action context
+ * @param {boolean} isPullRequest is the workflow triggered by a pull request?
+ */
+async function executeEndToEndTests(github, context, isPullRequest) {
+    const issue = context.issue;
+
+    if (!isPullRequest) {
+        console.log("[executeEndToEndTests] issues unsupported, skipping command execution.");
+        return;
+    } else if (owners.indexOf(context.actor) < 0) {
+        console.log("[executeEndToEndTests] user does not have privilege, skipping command execution.");
+        return;
+    }
+
+    // Get pull request
+    const pull = await github.pulls.get({
+        owner: issue.owner,
+        repo: issue.repo,
+        pull_number: issue.number
+    });
+
+    if (pull && pull.data) {
+         // Get commit id and repo from pull head
+         const clientPayload = {
+            pull_head_ref: pull.data.head.sha,
+            pull_head_repo: pull.data.head.repo.full_name,
+            command: "ok-to-e2e-test",
+            issue: issue,
+          };
+
+        // Fire repository_dispatch event to trigger e2e test
+        await github.repos.createDispatchEvent({
+            owner: issue.owner,
+            repo: issue.repo,
+            event_type: "e2e-test",
+            client_payload: clientPayload,
+        });
+
+        console.log(`[executeEndToEndTests] triggered E2E tests for ${JSON.stringify(clientPayload)}.`);
     }
 }
 
 
-module.exports = async ({ github, context }) => {
-    // list of owner who can control dapr-bot workflow
-    // TODO: Read owners from OWNERS file.
-    const owners = [
-        "yaron2",
-        "youngbupark",
-        "Haishi2016",
-        "lukekim",
-        "amanbha",
-        "msfussell",
-        "shalabhms",
-        "LMWF",
-        "artursouza",
-        "vinayada1",
-        "mukundansundar",
-        "wcs1only",
-        "orizohar",
-        "pruthvidhodda",
-        "mchmarny",
-        "tcnghia",
-        "berndverst",
-        "halspang",
-        "tanvigour",
-        "dmitsh",
-        "pkedy",
-        "CodeMonkeyLeet",
-        "XavierGeerinck",
-        "amulyavarote",
-        "shubham1172"
-    ];
-
+export default async ({ github, context }) => {
     const payload = context.payload;
     const issue = context.issue;
     const isFromPulls = !!payload.issue.pull_request;
     const commentBody = payload.comment.body;
-    if (!isFromPulls && commentBody && commentBody.indexOf("/assign") == 0) {
-        if (!issue.assignees || issue.assignees.length === 0) {
-            await github.issues.addAssignees({
-                owner: issue.owner,
-                repo: issue.repo,
-                issue_number: issue.number,
-                assignees: [context.actor],
-            })
-        }
+
+    if (!commentBody) {
+        console.log("[main] comment body not found, exiting.");
         return;
+    }
+
+    switch (commentBody) {
+        case "/assign":
+            executeAssign(github, context, isFromPulls);
+            break;
+        case "/ping-author":
+            executePingAuthor(github, context, isFromPulls);
+            break;
+        case "/ok-to-e2e-test":
+            executeEndToEndTests(github, context, isFromPulls);
+            break;
+        default:
+            console.log(`[main] command ${commentBody} not found, exiting.`);
+            break;
     }
 
     // the author of this issue is interacting with it
@@ -84,66 +198,7 @@ module.exports = async ({ github, context }) => {
                 owner: issue.owner,
                 repo: issue.repo,
                 labels: ['needs-team-attention']
-            })
-        })
-    }
-
-    // actions above this check are enabled for everyone.
-    if (owners.indexOf(context.actor) < 0) {
-        return;
-    }
-
-    // commands for GH issues
-    if (!isFromPulls && commentBody) {
-        if (commentBody.indexOf("/ping-author") == 0) {
-            // if there is a 'needs-team-attention' label, remove it.
-            await executeIfIssueHasLabel(github, issue, 'needs-team-attention', async () => {
-                await github.issues.removeLabel({
-                    issue_number: issue.number,
-                    owner: issue.owner,
-                    repo: issue.repo,
-                    name: 'needs-team-attention'
-                });
-            })
-            // Add new label
-            await github.issues.addLabels({
-                issue_number: issue.number,
-                owner: issue.owner,
-                repo: issue.repo,
-                labels: ['needs-author-feedback']
-            })
-            return;
-        }
-    }
-
-    // commands for GH pull requests
-    if (isFromPulls && commentBody) {
-        if (commentBody.indexOf("/ok-to-e2e-test") == 0) {
-            // Get pull request
-            const pull = await github.pulls.get({
-                owner: issue.owner,
-                repo: issue.repo,
-                pull_number: issue.number
             });
-            if (pull && pull.data) {
-                 // Get commit id and repo from pull head
-                 const clientPayload = {
-                    pull_head_ref: pull.data.head.sha,
-                    pull_head_repo: pull.data.head.repo.full_name,
-                    command: "ok-to-e2e-test",
-                    issue: issue,
-                  };
-
-                // Fire repository_dispatch event to trigger e2e test
-                await github.repos.createDispatchEvent({
-                    owner: issue.owner,
-                    repo: issue.repo,
-                    event_type: "e2e-test",
-                    client_payload: clientPayload,
-                });
-
-                console.log(`Trigger E2E test for ${JSON.stringify(clientPayload)}`);
-            }
-        }
+        });
     }
-}
+};
