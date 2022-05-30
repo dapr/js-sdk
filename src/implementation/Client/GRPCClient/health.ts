1070851381058023a39ada63518cc7688918d6e7
Xavier Geerinck <xavier.geerinck@gmail.com>
2022-05-30 14:01:11 +0200

    Utilize isSidecarStarted in health
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- src/implementation/Client/GRPCClient/health.ts
+++ src/implementation/Client/GRPCClient/health.ts
@@ -13,8 +13,6 @@ limitations under the License.
 
 import GRPCClient from './GRPCClient';
 import IClientHealth from '../../../interfaces/Client/IClientHealth';
-import { GetMetadataResponse } from '../../../proto/dapr/proto/runtime/v1/dapr_pb';
-import { Empty } from "google-protobuf/google/protobuf/empty_pb";
 
 // https://docs.dapr.io/reference/api/health_api/
 export default class GRPCClientHealth implements IClientHealth {
@@ -26,20 +24,6 @@ export default class GRPCClientHealth implements IClientHealth {
 
   // There is no gRPC implementation of /healthz, so we try to fetch the metadata
   async isHealthy(): Promise<boolean> {
-    return new Promise((resolve, _reject) => {
-      const client = this.client.getClient();
-
-      try {
-        client.getMetadata(new Empty(), (err, _res: GetMetadataResponse) => {
-          if (err) {
-            return resolve(false);
-          }
-
-          return resolve(true);
-        });
-      } catch (e) {
-        return resolve(false);
-      }
-    });
+    return this.client.isSidecarStarted();
   }
 }
