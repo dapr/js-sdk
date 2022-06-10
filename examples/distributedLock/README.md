aeedd280997573fe1710159123d717364f5e03f3
Amulya Varote <amulyavarote@microsoft.com>
2022-06-10 12:24:39 -0700

    Added distributed lock api support to javascript sdk
    Signed-off-by: Amulya Varote <amulyavarote@microsoft.com>

---
+++ examples/distributedLock/README.md
@@ -0,0 +1,78 @@
+# Example - Distributed Lock APIs
+
+This example demonstrates the Distributed Lock APIs in Dapr.
+It demonstrates the following APIs:
+- **Distributed Lock**: Try Lock
+- **Distributed Lock**: Unlock
+
+> **Note:** Make sure to use the latest proto bindings
+## Prerequisites
+
+- [Dapr CLI](https://docs.dapr.io/getting-started/install-dapr-cli/)
+- [Dapr JS SDK](https://docs.dapr.io/developing-applications/sdks/js/)
+- Initialize Dapr environment by pulling the code from master branch of [Dapr](https://github.com/dapr/dapr)
+
+## Overview
+This example shows the usage of two different Distributed Lock APIs, TryLock call and Unlock call.
+
+#### TryLock Example
+```javascript
+const tryLockResponse = await client.distributedLock.tryLock(storeName, resourceId, lockOwner, expiryInSeconds);
+```
+
+#### Unlock Example
+```javascript
+const unLockResponse = await client.distributedLock.unLock(storeName, resourceId, lockOwner);
+```
+
+### Start the DistributedLock TryLockApplication.
+
+Change directory to this folder:
+
+```bash
+cd examples/Client/DistributedLockApi/TryLockApplication
+npm install
+```
+
+To run the `TryLockApplication`, execute the following command:
+
+```bash
+dapr run --app-id distributedLock --app-protocol grpc --components-path ./Components npm run start
+```
+
+You should see the following output from the application:
+
+```
+== APP == { success: true }
+== APP == Unlock API response: Success
+== APP == Unlock API response when lock is not acquired: LockUnexist
+== APP == Acquired Lock? true
+```
+
+### Start the DistributedLock UnLockApplication.
+
+Run `UnLockApplication` after `TryLockApplication` is ran.
+
+Change directory to this folder:
+
+```bash
+cd examples/Client/DistributedLockApi/UnLockApplication
+npm install
+```
+
+To run the `UnLockApplication`, execute the following command:
+
+```bash
+dapr run --app-id distributedLock --app-protocol grpc --components-path ./Components npm run start
+```
+
+You should see the following output from the application:
+
+```
+== APP == Acquired Lock? false
+== APP == Lock cannot be acquired as it belongs to the other process
+== APP == Unlock API response when lock is acquired by a different process: LockBelongToOthers
+== APP == Acquired lock after the lock from the other process expired? true
+== APP == Unlock API response when lock is released after the expiry time: Success
+== APP == Unlock API response when lock is released after the expiry time and lock does not exist: LockUnexist
+```
--- src/proto/dapr/proto/runtime/v1/dapr_pb.d.ts
+++ src/proto/dapr/proto/runtime/v1/dapr_pb.d.ts
@@ -1218,3 +1218,106 @@ export namespace UnsubscribeConfigurationResponse {
         message: string,
     }
 }
+
+export class TryLockRequest extends jspb.Message { 
+    getStoreName(): string;
+    setStoreName(value: string): TryLockRequest;
+    getResourceId(): string;
+    setResourceId(value: string): TryLockRequest;
+    getLockOwner(): string;
+    setLockOwner(value: string): TryLockRequest;
+    getExpiryinseconds(): number;
+    setExpiryinseconds(value: number): TryLockRequest;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): TryLockRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: TryLockRequest): TryLockRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: TryLockRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): TryLockRequest;
+    static deserializeBinaryFromReader(message: TryLockRequest, reader: jspb.BinaryReader): TryLockRequest;
+}
+
+export namespace TryLockRequest {
+    export type AsObject = {
+        storeName: string,
+        resourceId: string,
+        lockOwner: string,
+        expiryinseconds: number,
+    }
+}
+
+export class TryLockResponse extends jspb.Message { 
+    getSuccess(): boolean;
+    setSuccess(value: boolean): TryLockResponse;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): TryLockResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: TryLockResponse): TryLockResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: TryLockResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): TryLockResponse;
+    static deserializeBinaryFromReader(message: TryLockResponse, reader: jspb.BinaryReader): TryLockResponse;
+}
+
+export namespace TryLockResponse {
+    export type AsObject = {
+        success: boolean,
+    }
+}
+
+export class UnlockRequest extends jspb.Message { 
+    getStoreName(): string;
+    setStoreName(value: string): UnlockRequest;
+    getResourceId(): string;
+    setResourceId(value: string): UnlockRequest;
+    getLockOwner(): string;
+    setLockOwner(value: string): UnlockRequest;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): UnlockRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: UnlockRequest): UnlockRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: UnlockRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): UnlockRequest;
+    static deserializeBinaryFromReader(message: UnlockRequest, reader: jspb.BinaryReader): UnlockRequest;
+}
+
+export namespace UnlockRequest {
+    export type AsObject = {
+        storeName: string,
+        resourceId: string,
+        lockOwner: string,
+    }
+}
+
+export class UnlockResponse extends jspb.Message { 
+    getStatus(): UnlockResponse.Status;
+    setStatus(value: UnlockResponse.Status): UnlockResponse;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): UnlockResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: UnlockResponse): UnlockResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: UnlockResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): UnlockResponse;
+    static deserializeBinaryFromReader(message: UnlockResponse, reader: jspb.BinaryReader): UnlockResponse;
+}
+
+export namespace UnlockResponse {
+    export type AsObject = {
+        status: UnlockResponse.Status,
+    }
+
+    export enum Status {
+    SUCCESS = 0,
+    LOCK_UNEXIST = 1,
+    LOCK_BELONG_TO_OTHERS = 2,
+    INTERNAL_ERROR = 3,
+    }
+
+}
