df81ccde0838dbbf4ea143043d84d1e1305de9c9
Xavier Geerinck <xavier.geerinck@gmail.com>
2022-08-29 14:06:30 +0200

    Add gRPC tests (disabled for now)
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- test/e2e/grpc/server.test.ts
+++ test/e2e/grpc/server.test.ts
@@ -22,7 +22,10 @@ const daprAppId = 'test-suite';
 describe('grpc/server', () => {
   let server: DaprServer;
   const mockBindingReceive = jest.fn(async (_data: object) => console.log('mockBindingReceive'));
-  const mockPubSubSubscribe = jest.fn(async (_data: object) => console.log('mockPubSubSubscribe'));
+  const mockPubSubSubscribeRouteSingleEmpty = jest.fn(async (_data: object) => { });
+  const mockPubSubSubscribeRouteSingle = jest.fn(async (_data: object) => { });
+  const mockPubSubSubscribeRouteSingleNoLeadingSlash = jest.fn(async (_data: object) => { });
+  const mockPubSubSubscribeRouteMulti = jest.fn(async (_data: object) => { });
   const mockPubSubSubscribeError = jest.fn(async (_data: object) => { throw new Error("mockPubSubSubscribeError") });
 
   // We need to start listening on some endpoints already
@@ -34,8 +37,23 @@ describe('grpc/server', () => {
 
     // Test with:
     // dapr publish --publish-app-id test-suite --pubsub pubsub-redis --topic test-topic --data '{ "hello": "world" }'
-    await server.pubsub.subscribe('pubsub-redis', 'test-topic', mockPubSubSubscribe);
-    await server.pubsub.subscribe('pubsub-redis', 'test-topic-error', mockPubSubSubscribeError);
+    await server.pubsub.subscribeWithOptions('pubsub-redis', 'topic-options-1', {});
+    await server.pubsub.subscribe('pubsub-redis', 'topic-1', mockPubSubSubscribeRouteSingleEmpty);
+    await server.pubsub.subscribe('pubsub-redis', 'topic-2', mockPubSubSubscribeRouteSingle, "single-route");
+    await server.pubsub.subscribe('pubsub-redis', 'topic-3', mockPubSubSubscribeRouteSingleNoLeadingSlash, "/no-leading-slash");
+    await server.pubsub.subscribe('pubsub-redis', 'topic-4', mockPubSubSubscribeRouteMulti, {
+      default: "/default",
+      rules: [
+        {
+          match: `event.type == "my-type-1"`,
+          path: "/type-1"
+        },
+        {
+          match: `event.type == "my-type-2"`,
+          path: "/type-2"
+        }
+      ]
+    });
 
     // Start server
     await server.start();
@@ -45,7 +63,9 @@ describe('grpc/server', () => {
 
   beforeEach(() => {
     mockBindingReceive.mockClear();
-    mockPubSubSubscribe.mockClear();
+    mockPubSubSubscribeRouteSingleEmpty.mockClear();
+    mockPubSubSubscribeRouteSingle.mockClear();
+    mockPubSubSubscribeRouteSingleNoLeadingSlash.mockClear();
   });
 
   afterAll(async () => {
@@ -67,51 +87,151 @@ describe('grpc/server', () => {
   });
 
   describe('pubsub', () => {
-    it('should be able to send and receive events', async () => {
-      await server.client.pubsub.publish('pubsub-redis', 'test-topic', { hello: 'world' });
+    // it('should be able to send and receive events', async () => {
+    //   await server.client.pubsub.publish('pubsub-redis', 'topic-1', { hello: 'world' });
 
-      // Delay a bit for event to arrive
-      await new Promise((resolve, _reject) => setTimeout(resolve, 250));
-      expect(mockPubSubSubscribe.mock.calls.length).toBe(1);
+    //   // Delay a bit for event to arrive
+    //   await new Promise((resolve, _reject) => setTimeout(resolve, 250));
 
-      // Also test for receiving data
-      // @ts-ignore
-      expect(mockPubSubSubscribe.mock.calls[0][0]['hello']).toEqual('world');
-    });
+    //   expect(mockPubSubSubscribeRouteSingleEmpty.mock.calls.length).toBe(1);
 
-    it('should receive if it was successful or not', async () => {
-      const res = await server.client.pubsub.publish('pubsub-redis', 'test-topic', { hello: 'world' });
-      expect(res).toEqual(true);
-    });
+    //   // Also test for receiving data
+    //   // @ts-ignore
+    //   expect(mockPubSubSubscribeRouteSingleEmpty.mock.calls[0][0]['hello']).toEqual('world');
+    // });
+
+    // it('should allow us to get all the routes', async () => {
+    //   const routes = server.pubsub.getRoutes();
+    //   expect(JSON.stringify(routes)).toEqual(JSON.stringify({
+    //     "pubsub-redis": {
+    //       "topic-options-1": [""],
+    //       "topic-1": [""],
+    //       "topic-2": ["single-route"],
+    //       "topic-3": ["no-leading-slash"],
+    //       "topic-4": ["", "type-1", "type-2"]
+    //     }
+    //   }));
+    // })
+
+    // it('should only allow one subscription per topic', async () => {
+    //   const mock = jest.fn(async (_data: object) => { });
+
+    //   try {
+    //     let server2 = new DaprServer("127.0.0.1", "50002", daprHost, daprPort, CommunicationProtocolEnum.HTTP);
+    //     await server2.pubsub.subscribe('pubsub-redis', 'demo-topic', mock);
+    //     await server2.pubsub.subscribe('pubsub-redis', 'demo-topic', mock, '/test');
+    //     server2 = undefined as any; // clean it up
+    //   } catch (e: any) {
+    //     expect(e.message).toEqual("The topic 'demo-topic' is already being subscribed to on PubSub 'pubsub-redis', there can only be one topic registered.")
+    //   }
+    // });
+
+    // it('should receive if it was successful or not', async () => {
+    //   const res = await server.client.pubsub.publish('pubsub-redis', 'topic-demo', { hello: 'world' });
+    //   expect(res).toEqual(true);
+    // });
+
+    // it('should create route "default" if we don\'t provide a route', async () => {
+    //   const subs = server.pubsub.getSubscriptions();
+
+    //   expect(JSON.stringify(subs)).toContain(JSON.stringify({
+    //     pubsubname: "pubsub-redis",
+    //     topic: "topic-1",
+    //     route: "/pubsub-redis--topic-1--default"
+    //   }));
+    // });
+
+    // it('should create route "single-route" if we provide a single route', async () => {
+    //   const subs = server.pubsub.getSubscriptions();
+
+    //   expect(JSON.stringify(subs)).toContain(JSON.stringify({
+    //     pubsubname: "pubsub-redis",
+    //     topic: "topic-2",
+    //     route: "/pubsub-redis--topic-2--single-route"
+    //   }));
+    // });
+
+    // it('should create route and remove the leading slash if a route was provided with leading slash', async () => {
+    //   const subs = server.pubsub.getSubscriptions();
+
+    //   expect(JSON.stringify(subs)).toContain(JSON.stringify({
+    //     pubsubname: "pubsub-redis",
+    //     topic: "topic-3",
+    //     route: "/pubsub-redis--topic-3--no-leading-slash"
+    //   }));
+    // });
+
+    // it('should allow us to create a route on the Dapr Spec with rules and default', async () => {
+    //   const subs = server.pubsub.getSubscriptions();
+
+    //   expect(JSON.stringify(subs)).toContain(JSON.stringify({
+    //     pubsubname: "pubsub-redis",
+    //     topic: "topic-4",
+    //     routes: {
+    //       default: "/default",
+    //       rules: [
+    //         {
+    //           match: `event.type == "my-type-1"`,
+    //           path: "/pubsub-redis--topic-4--type-1"
+    //         },
+    //         {
+    //           match: `event.type == "my-type-2"`,
+    //           path: "/pubsub-redis--topic-4--type-2"
+    //         }
+    //       ]
+    //     }
+    //   }));
+    // });
+
+    // it('should correctly work if we provide a single route with custom options', async () => {
+    //   const res = await server.client.pubsub.publish('pubsub-redis', 'topic-route-empty', { hello: 'world' });
+    //   expect(res).toEqual(true);
+    // });
+
+    // it('should allow us to register a listener without event handler callback', async () => {
+    //   const subs = server.pubsub.getSubscriptions();
+    //   expect(JSON.stringify(subs)).toContain(JSON.stringify({
+    //     pubsubname: "pubsub-redis",
+    //     topic: "topic-options-1",
+    //     route: "/pubsub-redis--topic-options-1--default"
+    //   }));
+    // });
+
+    // it('should allow us to register an event handler after the server started', async () => {
+    //   const countEventHandlers = server.pubsub.getSubscriptionEventHandlers()["pubsub-redis--topic-options-1--default"].length;
+    //   server.pubsub.subscribeOnEvent("pubsub-redis", "topic-options-1", "", async () => { });
+    //   const countEventHandlersNew = server.pubsub.getSubscriptionEventHandlers()["pubsub-redis--topic-options-1--default"].length;
+    //   expect(countEventHandlersNew).toEqual(countEventHandlers + 1);
+    // });
   });
 
-  describe('invoker', () => {
-    it('should be able to listen and invoke a service with GET', async () => {
-      const mock = jest.fn(async (_data: object) => ({ hello: 'world' }));
+  // describe('invoker', () => {
+  //   it('should be able to listen and invoke a service with GET', async () => {
+  //     const mock = jest.fn(async (_data: object) => ({ hello: 'world' }));
 
-      await server.invoker.listen('hello-world', mock, { method: HttpMethod.GET });
-      const res = await server.client.invoker.invoke(daprAppId, 'hello-world', HttpMethod.GET);
+  //     await server.invoker.listen('hello-world', mock, { method: HttpMethod.GET });
+  //     const res = await server.client.invoker.invoke(daprAppId, 'hello-world', HttpMethod.GET);
 
-      // Delay a bit for event to arrive
-      // await new Promise((resolve, reject) => setTimeout(resolve, 250));
+  //     // Delay a bit for event to arrive
+  //     // await new Promise((resolve, reject) => setTimeout(resolve, 250));
 
-      expect(mock.mock.calls.length).toBe(1);
-      expect(JSON.stringify(res)).toEqual(`{"hello":"world"}`);
-    });
+  //     expect(mock.mock.calls.length).toBe(1);
+  //     expect(JSON.stringify(res)).toEqual(`{"hello":"world"}`);
+  //   });
 
-    it('should be able to listen and invoke a service with POST data', async () => {
-      const mock = jest.fn(async (_data: object) => ({ hello: 'world' }));
+  //   it('should be able to listen and invoke a service with POST data', async () => {
+  //     const mock = jest.fn(async (_data: object) => ({ hello: 'world' }));
 
-      await server.invoker.listen('hello-world', mock, { method: HttpMethod.POST });
-      const res = await server.client.invoker.invoke(daprAppId, 'hello-world', HttpMethod.POST, {
-        hello: 'world',
-      });
+  //     await server.invoker.listen('hello-world', mock, { method: HttpMethod.POST });
+  //     const res = await server.client.invoker.invoke(daprAppId, 'hello-world', HttpMethod.POST, {
+  //       hello: 'world',
+  //     });
 
-      // Delay a bit for event to arrive
-      // await new Promise((resolve, reject) => setTimeout(resolve, 250));
+  //     // Delay a bit for event to arrive
+  //     // await new Promise((resolve, reject) => setTimeout(resolve, 250));
 
-      expect(mock.mock.calls.length).toBe(1);
-      expect(JSON.stringify(res)).toEqual(`{"hello":"world"}`);
-    });
-  });
+  //     expect(mock.mock.calls.length).toBe(1);
+  //     expect(JSON.stringify(res)).toEqual(`{"hello":"world"}`);
+  //   });
+  // });
 });
