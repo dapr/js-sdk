aecd33a1a4f3082fecd156a599f95b0f3025a0f3
pathan salman khan <pathans@geekyants.com>
2022-09-30 17:00:15 +0530

    added the doc comment to invokeroption
    Signed-off-by: pathan salman khan <pathans@geekyants.com>

--- src/implementation/Client/GRPCClient/invoker.ts
+++ src/implementation/Client/GRPCClient/invoker.ts
@@ -20,6 +20,7 @@ import { InvokeServiceRequest } from '../../../proto/dapr/proto/runtime/v1/dapr_
 import * as HttpVerbUtil from "../../../utils/HttpVerb.util";
 import IClientInvoker from '../../../interfaces/Client/IClientInvoker';
 import * as SerializerUtil from "../../../utils/Serializer.util"
+import { InvokerOptions } from "../../../types/InvokerOptions.type";
 
 // https://docs.dapr.io/reference/api/service_invocation_api/
 export default class GRPCClientInvoker implements IClientInvoker {
@@ -30,20 +31,23 @@ export default class GRPCClientInvoker implements IClientInvoker {
   }
 
   // @todo: should return a specific typed Promise<TypeInvokerInvokeResponse> instead of Promise<nothing>
-  async invoke(appId: string, methodName: string, method: HttpMethod = HttpMethod.GET, data: object = {}): Promise<object> {
+  async invoke(appId: string, methodName: string, method: HttpMethod = HttpMethod.GET, data?: object,options:InvokerOptions={}): Promise<object> {
+    
+    const headers= options.headers!=undefined?options.headers:{}
+ 
     const fetchOptions = {
-      method
+      method,
+      headers
     };
 
     if (method !== HttpMethod.GET) {
-      // @ts-ignore
-      fetchOptions.headers = {
-        'Content-Type': 'application/json'
-      };
+       //@ts-ignore
+      fetchOptions.headers['Content-Type'] = 'application/json';
     }
 
-    if (method !== HttpMethod.GET && data !== {}) {
-      // @ts-ignore
+
+    if (method !== HttpMethod.GET && data !== undefined) {
+      //@ts-ignore
       fetchOptions.body = JSON.stringify(data);
     }
 
@@ -55,7 +59,7 @@ export default class GRPCClientInvoker implements IClientInvoker {
     httpExtension.setVerb(HttpVerbUtil.convertHttpVerbStringToNumber(method));
 
     const msgSerialized = new Any();
-    const {serializedData, contentType} = SerializerUtil.serializeGrpc(data);
+    const { serializedData, contentType } = SerializerUtil.serializeGrpc(data);
     msgSerialized.setValue(serializedData);
 
     const msgInvoke = new InvokeRequest();
@@ -82,12 +86,14 @@ export default class GRPCClientInvoker implements IClientInvoker {
           const parsedResData = JSON.parse(resData);
           return resolve(parsedResData);
         } catch (e) {
-          throw new Error(JSON.stringify({
-            error: "COULD_NOT_PARSE_RESULT",
-            error_msg: `Could not parse the returned resultset: ${resData}`
-          }));
+          throw new Error(
+            JSON.stringify({
+              error: "COULD_NOT_PARSE_RESULT",
+              error_msg: `Could not parse the returned resultset: ${resData}`,
+            }),
+          );
         }
-      })
-    })
+      });
+    });
   }
 }
