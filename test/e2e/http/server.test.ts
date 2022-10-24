a4383cde73e884f8469775ac22f007e40677f74d
Xavier Geerinck <xavier.geerinck@gmail.com>
2022-10-24 10:23:08 +0200

    Remove unused var
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- test/e2e/http/server.test.ts
+++ test/e2e/http/server.test.ts
@@ -11,7 +11,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import fetch from "node-fetch";
 import { CommunicationProtocolEnum, DaprServer, HttpMethod } from "../../../src";
 
 const serverHost = "127.0.0.1";
