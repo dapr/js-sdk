46f677de965f23364e2a78c043f73625fd7d8a3f
Shubham Sharma <shubhash@microsoft.com>
2022-12-14 19:51:28 +0530

    x
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- test/e2e/grpc/server.test.ts
+++ test/e2e/grpc/server.test.ts
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { CommunicationProtocolEnum, ConsoleLoggerService, DaprServer, HttpMethod } from "../../../src";
+import { CommunicationProtocolEnum, DaprServer, HttpMethod } from "../../../src";
 
 const serverHost = "localhost";
 const serverPort = "50001";
