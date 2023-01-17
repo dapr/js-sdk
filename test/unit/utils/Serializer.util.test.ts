0a77aff5b8181af442b6ae79006fdd56409d9c08
Xavier Geerinck <xavier.geerinck@gmail.com>
2023-01-17 11:03:29 +0100

    Add HTTP Serializer tests
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- test/unit/utils/Serializer.util.test.ts
+++ test/unit/utils/Serializer.util.test.ts
@@ -14,41 +14,83 @@ limitations under the License.
 import * as SerializerUtil from "../../../src/utils/Serializer.util";
 
 describe("serializer", () => {
-  it("Object should be serialized to Buffer as application/json", () => {
-    const data = SerializerUtil.serializeGrpc({ Hello: "World" });
-    expect(Buffer.compare(data.serializedData, Buffer.from(JSON.stringify({ Hello: "World" })))).toEqual(0);
-    expect(data.contentType).toEqual("application/json");
+  describe("grpc", () => {
+    it("Object should be serialized to Buffer as application/json", () => {
+      const data = SerializerUtil.serializeGrpc({ Hello: "World" });
+      expect(Buffer.compare(data.serializedData, Buffer.from(JSON.stringify({ Hello: "World" })))).toEqual(0);
+      expect(data.contentType).toEqual("application/json");
+    });
+
+    it("Object in CloudEvent Format should be serialized to Buffer as application/cloudevents+json", () => {
+      const obj = {
+        id: "demo",
+        source: "demo",
+        type: "demo",
+        specversion: "demo",
+      };
+
+      const data = SerializerUtil.serializeGrpc(obj);
+
+      expect(Buffer.compare(data.serializedData, Buffer.from(JSON.stringify(obj)))).toEqual(0);
+      expect(data.contentType).toEqual("application/cloudevents+json");
+    });
+
+    it("String should be serialized as text/plain and as Buffer", () => {
+      const data = SerializerUtil.serializeGrpc("hello-world");
+      expect(data.serializedData).toEqual(Buffer.from("hello-world"));
+      expect(data.contentType).toEqual("text/plain");
+    });
+
+    it("Buffer should be serialized to Buffer as application/octet-stream", () => {
+      const data = SerializerUtil.serializeGrpc(Buffer.from("hello-world"));
+      expect(Buffer.compare(data.serializedData, Buffer.from("hello-world", "utf-8"))).toEqual(0);
+      expect(data.contentType).toEqual("application/octet-stream");
+    });
+
+    it("Buffer object should not be serialized again", () => {
+      const data = SerializerUtil.serializeGrpc(Buffer.from("Hello World"));
+      expect(Buffer.compare(data.serializedData, Buffer.from("Hello World"))).toEqual(0);
+      expect(data.contentType).toEqual("application/octet-stream");
+    });
   });
 
-  it("Object in CloudEvent Format should be serialized to Buffer as application/cloudevents+json", () => {
-    const obj = {
-      id: "demo",
-      source: "demo",
-      type: "demo",
-      specversion: "demo",
-    };
+  describe("http", () => {
+    it("Object should be serialized to string as application/json", () => {
+      const data = SerializerUtil.serializeHttp({ Hello: "World" });
+      expect(data.serializedData).toEqual(JSON.stringify({ Hello: "World" }));
+      expect(data.contentType).toEqual("application/json");
+    });
 
-    const data = SerializerUtil.serializeGrpc(obj);
+    it("Object in CloudEvent Format should be serialized to string as application/cloudevents+json", () => {
+      const obj = {
+        id: "demo",
+        source: "demo",
+        type: "demo",
+        specversion: "demo",
+      };
 
-    expect(Buffer.compare(data.serializedData, Buffer.from(JSON.stringify(obj)))).toEqual(0);
-    expect(data.contentType).toEqual("application/cloudevents+json");
-  });
+      const data = SerializerUtil.serializeHttp(obj);
 
-  it("String should be serialized as text/plain and as Buffer", () => {
-    const data = SerializerUtil.serializeGrpc("hello-world");
-    expect(data.serializedData).toEqual(Buffer.from("hello-world"));
-    expect(data.contentType).toEqual("text/plain");
-  });
+      expect(data.serializedData).toEqual(JSON.stringify(obj));
+      expect(data.contentType).toEqual("application/cloudevents+json");
+    });
 
-  it("Buffer should be serialized to Buffer as application/octet-stream", () => {
-    const data = SerializerUtil.serializeGrpc(Buffer.from("hello-world"));
-    expect(Buffer.compare(data.serializedData, Buffer.from("hello-world", "utf-8"))).toEqual(0);
-    expect(data.contentType).toEqual("application/octet-stream");
-  });
+    it("String should be serialized as text/plain and as Buffer", () => {
+      const data = SerializerUtil.serializeHttp("hello-world");
+      expect(data.serializedData).toEqual("hello-world");
+      expect(data.contentType).toEqual("text/plain");
+    });
 
-  it("Buffer object should not be serialized again", () => {
-    const data = SerializerUtil.serializeGrpc(Buffer.from("Hello World"));
-    expect(Buffer.compare(data.serializedData, Buffer.from("Hello World"))).toEqual(0);
-    expect(data.contentType).toEqual("application/octet-stream");
+    it("Buffer should be serialized to Buffer as application/octet-stream", () => {
+      const data = SerializerUtil.serializeHttp(Buffer.from("hello-world"));
+      expect(data.serializedData).toEqual(Buffer.from("hello-world", "utf-8"));
+      expect(data.contentType).toEqual("application/octet-stream");
+    });
+
+    it("Buffer object should not be serialized again", () => {
+      const data = SerializerUtil.serializeHttp(Buffer.from("Hello World"));
+      expect(data.serializedData).toEqual(Buffer.from("Hello World"));
+      expect(data.contentType).toEqual("application/octet-stream");
+    });
   });
 });
