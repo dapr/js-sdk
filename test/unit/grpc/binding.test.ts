9c6b3c3327847c1053153056bd66943c8f667bde
Shubham Sharma <shubhash@microsoft.com>
2023-01-29 12:25:28 +0530

    Prettify
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- test/unit/grpc/binding.test.ts
+++ test/unit/grpc/binding.test.ts
@@ -15,51 +15,55 @@ import GRPCClientBinding from "../../../src/implementation/Client/GRPCClient/bin
 import { InvokeBindingRequest } from "../../../src/proto/dapr/proto/runtime/v1/dapr_pb";
 
 describe("grpc/binding", () => {
-    describe("send should call invokeBinding with correct arguments", () => {
-        const getMockClient = (requests: any[]) => {
-            const mockInvokeBinding = (req: InvokeBindingRequest, callback: any) => {
-                requests.push(req);
-                callback(null, {
-                    getData: () => { return "" },
-                    getMetadataMap: () => { return {} }
-                });
-            }
-            const mockClient = {
-                getClient: () => {
-                    return { invokeBinding: mockInvokeBinding }
-                }
-            } as any;
-            return mockClient;
-        }
-
-        it("should create the correct binding request", async () => {
-            const requests: InvokeBindingRequest[] = [];
-            const grpcClientBinding = new GRPCClientBinding(getMockClient(requests));
-            await grpcClientBinding.send("my-binding", "create", { key: "value" }, { mKey: "mValue" });
-
-            // Check the request
-            expect(requests.length).toBe(1);
-            const binding = requests[0];
-            expect(binding.getName()).toBe("my-binding");
-            expect(binding.getOperation()).toBe("create");
-            expect(binding.getData()).toStrictEqual(Buffer.from(JSON.stringify({ key: "value" })));
-            expect(binding.getMetadataMap().getLength()).toBe(1);
-            expect(binding.getMetadataMap().get("mKey")).toBe("mValue");
+  describe("send should call invokeBinding with correct arguments", () => {
+    const getMockClient = (requests: any[]) => {
+      const mockInvokeBinding = (req: InvokeBindingRequest, callback: any) => {
+        requests.push(req);
+        callback(null, {
+          getData: () => {
+            return "";
+          },
+          getMetadataMap: () => {
+            return {};
+          },
         });
+      };
+      const mockClient = {
+        getClient: () => {
+          return { invokeBinding: mockInvokeBinding };
+        },
+      } as any;
+      return mockClient;
+    };
 
-        it("should skip data when it's not present", async () => {
-            const requests: InvokeBindingRequest[] = [];
-            const grpcClientBinding = new GRPCClientBinding(getMockClient(requests));
-            await grpcClientBinding.send("my-binding", "create", undefined, { mKey: "mValue" });
+    it("should create the correct binding request", async () => {
+      const requests: InvokeBindingRequest[] = [];
+      const grpcClientBinding = new GRPCClientBinding(getMockClient(requests));
+      await grpcClientBinding.send("my-binding", "create", { key: "value" }, { mKey: "mValue" });
 
-            // Check the request
-            expect(requests.length).toBe(1);
-            const binding = requests[0];
-            expect(binding.getName()).toBe("my-binding");
-            expect(binding.getOperation()).toBe("create");
-            expect(binding.getData()).toStrictEqual("");
-            expect(binding.getMetadataMap().getLength()).toBe(1);
-            expect(binding.getMetadataMap().get("mKey")).toBe("mValue");
-        });
+      // Check the request
+      expect(requests.length).toBe(1);
+      const binding = requests[0];
+      expect(binding.getName()).toBe("my-binding");
+      expect(binding.getOperation()).toBe("create");
+      expect(binding.getData()).toStrictEqual(Buffer.from(JSON.stringify({ key: "value" })));
+      expect(binding.getMetadataMap().getLength()).toBe(1);
+      expect(binding.getMetadataMap().get("mKey")).toBe("mValue");
     });
+
+    it("should skip data when it's not present", async () => {
+      const requests: InvokeBindingRequest[] = [];
+      const grpcClientBinding = new GRPCClientBinding(getMockClient(requests));
+      await grpcClientBinding.send("my-binding", "create", undefined, { mKey: "mValue" });
+
+      // Check the request
+      expect(requests.length).toBe(1);
+      const binding = requests[0];
+      expect(binding.getName()).toBe("my-binding");
+      expect(binding.getOperation()).toBe("create");
+      expect(binding.getData()).toStrictEqual("");
+      expect(binding.getMetadataMap().getLength()).toBe(1);
+      expect(binding.getMetadataMap().get("mKey")).toBe("mValue");
+    });
+  });
 });
