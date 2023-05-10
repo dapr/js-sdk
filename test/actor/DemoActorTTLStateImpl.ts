508abe21a50cb82637069187acb793c9d9bc6a35
Salman khan <salmankhanprs@gmail.com>
2023-05-10 10:49:29 +0530

    updated the function signatures
    Signed-off-by: Salman khan <salmankhanprs@gmail.com>

--- test/actor/DemoActorTTLStateImpl.ts
+++ test/actor/DemoActorTTLStateImpl.ts
@@ -15,23 +15,23 @@ import { AbstractActor } from "../../src";
 import DemoActorTTLStateInterface from "./DemoActorTTLStateInterface";
 
 export default class DemoActorTTLStateImpl extends AbstractActor implements DemoActorTTLStateInterface {
-    async init(): Promise<string> {
-        await this.getStateManager().getOrAddState("data", "Hello World!");
+  async init(): Promise<string> {
+    await this.getStateManager().getOrAddState("data", "Hello World!");
 
-        return "state initialized";
-    }
+    return "state initialized";
+  }
 
-    async getState<T>(): Promise<T | null> {
-        return await this.getStateManager<T>().getState("data");
-    }
+  async getState<T>(): Promise<T | null> {
+    return await this.getStateManager<T>().getState("data");
+  }
 
-    async deleteState(key: string): Promise<void> {
-        await this.getStateManager().removeState(key);
-        await this.getStateManager().saveState();
-    }
+  async deleteState(key: string): Promise<void> {
+    await this.getStateManager().removeState(key);
+    await this.getStateManager().saveState();
+  }
 
-    async setStateWithTTL<T>(key: string, value: T, ttl: number): Promise<void> {
-        await this.getStateManager<T>().setStateWithTTL(key, value, ttl);
-        await this.getStateManager().saveState();
-    }
+  async setStateWithTTL(key: string, value: any, ttl: number): Promise<void> {
+    await this.getStateManager().setStateWithTTL(key, value, ttl);
+    await this.getStateManager().saveState();
+  }
 }
--- test/actor/DemoActorTTLStateInterface.ts
+++ test/actor/DemoActorTTLStateInterface.ts
@@ -15,5 +15,5 @@ export default interface DemoActorTTLStateInterface {
   init(): Promise<string>;
   getState<T>(): Promise<T | null>;
   deleteState(key: string): Promise<void>;
-  setStateWithTTL<T>(key: string, value: T, ttl: number): Promise<void>;
+  setStateWithTTL(key: string, value: any, ttl: number): Promise<void>;
 }
