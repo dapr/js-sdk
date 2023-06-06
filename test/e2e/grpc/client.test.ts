6b159b368bb1c0ce0b7ad5f2cee529f90f5cf1ad
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-06-06 13:06:31 -0700

    Added E2E tests
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- test/e2e/grpc/client.test.ts
+++ test/e2e/grpc/client.test.ts
@@ -11,6 +11,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { join } from "node:path";
+import { readFile } from "node:fs/promises";
+import { Readable } from "node:stream";
 import * as grpc from "@grpc/grpc-js";
 import { CommunicationProtocolEnum, DaprClient, LogLevel } from "../../../src";
 import { SubscribeConfigurationResponse } from "../../../src/types/configuration/SubscribeConfigurationResponse";
@@ -53,7 +56,7 @@ describe("grpc/client", () => {
     });
   });
 
-  describe("Proxy", () => {
+  describe("proxy", () => {
     it("should allow to use a proxy builder to proxy a gRPC request", async () => {
       let mockMetadataRes: grpc.Metadata = new grpc.Metadata();
       const mockInterceptor = jest.fn((options: grpc.InterceptorOptions, nextCall: NextCall): grpc.InterceptingCall => {
@@ -171,6 +174,124 @@ describe("grpc/client", () => {
     it("should be able to correctly fetch the secrets in bulk", async () => {
       const res = await client.secret.getBulk("secret-envvars");
       expect(Object.keys(res).length).toBeGreaterThan(1);
+    });
+  });
+
+  describe("crypto", () => {
+    let plaintextFile: Buffer;
+
+    it("read test file", async () => {
+      // Read the test file in memory
+      // This is in its own test case because we can't have async code in a describe() block
+      plaintextFile = await readFile(join(__dirname, "../../", "fixtures/plaintext.txt"));
+    });
+
+    it("should be able to encrypt and decrypt a stream", async () => {
+      // First, encrypt the file as a stream, and read the data in memory
+      const encStream = Readable.from(plaintextFile).pipe(
+        await client.crypto.encrypt({
+          componentName: "crypto-local",
+          keyName: "symmetric256",
+          keyWrapAlgorithm: "A256KW",
+        }),
+      );
+      const enc = await readStreamToBuffer(encStream);
+
+      // The output is at least partly random, so we can't make a lot of assertions
+      // We will test it more when decrypting it
+      expect(enc.length).toBeGreaterThan(1000);
+      expect(enc.slice(0, 15).toString("utf8")).toEqual("dapr.io/enc/v1\n");
+
+      // Decrypt the stream
+      const decStream = Readable.from(enc).pipe(
+        await client.crypto.decrypt({
+          componentName: "crypto-local",
+        }),
+      );
+      const dec = await readStreamToBuffer(decStream);
+
+      // dec should be the same as the plaintext file
+      expect(dec).toEqual(plaintextFile);
+    });
+
+    // Used to encrypt data, accepting the various types that client.crypto.encrypt accepts
+    const doEncrypt = async function (data: Buffer | ArrayBuffer | ArrayBufferView | string): Promise<Buffer> {
+      return client.crypto.encrypt(data, {
+        componentName: "crypto-local",
+        keyName: "symmetric256",
+        keyWrapAlgorithm: "A256KW",
+      });
+    };
+
+    // Used to decryot data, accepting the various types that client.crypto.decrypt accepts
+    const doDecrypt = async function (data: Buffer | ArrayBuffer | ArrayBufferView): Promise<Buffer> {
+      return client.crypto.decrypt(data, {
+        componentName: "crypto-local",
+      });
+    };
+
+    it("should be able to encrypt and decrypt Buffer's", async () => {
+      // Encrypt the data as a Buffer
+      const enc = await doEncrypt(plaintextFile);
+
+      // Basic assertions
+      expect(Buffer.isBuffer(enc)).toBeTruthy();
+      expect(enc.length).toBeGreaterThan(1000);
+      expect(enc.slice(0, 15).toString("utf8")).toEqual("dapr.io/enc/v1\n");
+
+      // Decrypt the data as Buffer
+      const dec = await doDecrypt(enc);
+
+      // dec should be the same as the plaintext file
+      expect(dec).toEqual(plaintextFile);
+    });
+
+    it("should be able to encrypt and decrypt strings", async () => {
+      // Encrypt the data as a string
+      const enc = await doEncrypt(plaintextFile.toString("utf8"));
+
+      // Basic assertions
+      expect(Buffer.isBuffer(enc)).toBeTruthy();
+      expect(enc.length).toBeGreaterThan(1000);
+      expect(enc.slice(0, 15).toString("utf8")).toEqual("dapr.io/enc/v1\n");
+
+      // Decrypt the data as a Buffer (we don't use strings for binary data)
+      const dec = await doDecrypt(enc);
+
+      // dec should be the same as the plaintext file
+      expect(dec).toEqual(plaintextFile);
+    });
+
+    it("should be able to encrypt and decrypt ArrayBuffer's", async () => {
+      // Encrypt the data as an ArrayBuffer
+      const enc = await doEncrypt(plaintextFile.buffer);
+
+      // Basic assertions
+      expect(Buffer.isBuffer(enc)).toBeTruthy();
+      expect(enc.length).toBeGreaterThan(1000);
+      expect(enc.slice(0, 15).toString("utf8")).toEqual("dapr.io/enc/v1\n");
+
+      // Decrypt the data as an ArrayBuffer
+      const dec = await doDecrypt(enc.buffer);
+
+      // dec should be the same as the plaintext file
+      expect(dec).toEqual(plaintextFile);
+    });
+
+    it("should be able to encrypt and decrypt typed arrays", async () => {
+      // Encrypt the data as an ArrayBuffer
+      const enc = await doEncrypt(new Uint8Array(plaintextFile.buffer));
+
+      // Basic assertions
+      expect(Buffer.isBuffer(enc)).toBeTruthy();
+      expect(enc.length).toBeGreaterThan(1000);
+      expect(enc.slice(0, 15).toString("utf8")).toEqual("dapr.io/enc/v1\n");
+
+      // Decrypt the data as an ArrayBuffer
+      const dec = await doDecrypt(new Uint8Array(enc.buffer));
+
+      // dec should be the same as the plaintext file
+      expect(dec).toEqual(plaintextFile);
     });
   });
 
@@ -310,3 +431,16 @@ describe("grpc/client", () => {
     });
   });
 });
+
+/**
+ * Reads all data from a readable stream in full, returning a buffer.
+ * This is not the most efficient way to process or even read a stream, but it's ok for testing purposes.
+ * @param stream Readable stream.
+ */
+async function readStreamToBuffer(stream: Readable): Promise<Buffer> {
+  const chunks: Buffer[] = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks);
+}
