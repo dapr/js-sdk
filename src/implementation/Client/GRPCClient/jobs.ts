340b25181548e915a72756fba7375db26048a5be
Alexander Trauzzi <acj@trauzzi.me>
2025-03-17 08:34:15 -0500

    Temporarily disable some eslint checks for builds.
    Signed-off-by: Alexander Trauzzi <acj@trauzzi.me>

--- src/implementation/Client/GRPCClient/jobs.ts
+++ src/implementation/Client/GRPCClient/jobs.ts
@@ -11,6 +11,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// todo: remove after implementation
+/* eslint-disable */
+
 import IClientJobs from "../../../interfaces/Client/IClientJobs";
 import GRPCClient from "./GRPCClient";
 import { JobSchedule } from "../../../types/jobs/JobSchedule.type";
