19d89127f4597bb6f1a6109bf3bef9c1df485bc2
Constantin Chirila <constantin@diagrid.io>
2025-08-14 12:25:13 +0100

    wip
    Signed-off-by: Constantin Chirila <constantin@diagrid.io>

--- test/e2e/grpc/server.test.ts
+++ test/e2e/grpc/server.test.ts
@@ -91,7 +91,7 @@ describe("grpc/server", () => {
       await server.client.binding.send("binding-mqtt", "create", { hello: "world" });
 
       // Delay a bit for event to arrive
-      await new Promise((resolve, _reject) => setTimeout(resolve, 1000));
+      await new Promise((resolve, _reject) => setTimeout(resolve, 2000));
       expect(mockBindingReceive.mock.calls.length).toBe(1);
 
       // Also test for receiving data
