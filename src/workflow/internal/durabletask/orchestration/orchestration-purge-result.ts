0d1f2d6a108c7b00ddf0db39273fb0d48e6735dd
Samantha Coyle <sam@diagrid.io>
2026-04-14 14:58:54 -0500

    style: add back microsoft license
    Signed-off-by: Samantha Coyle <sam@diagrid.io>

--- src/workflow/internal/durabletask/orchestration/orchestration-purge-result.ts
+++ src/workflow/internal/durabletask/orchestration/orchestration-purge-result.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 /*
 Copyright 2026 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
